79cbc5448acfd32e0986070fe31309f87bef2dc7
Mhmad-jamal <mohammed52008@hotmail.com>
2023-04-30 10:49:48 +0300

    update

---
+++ lang/en/master.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'store' => 'store',
+    'home'=>'home',
+    'about'=>'About us',
+    'contact'=>'contact us',
+    'ourNews'=>'Our news',
+    'social_sites' => 'our social sites',
+    'facebook' => 'facebook',
+    'instagram' => 'instagram',
+    'tiktok' => 'tiktok',
+    'youtube' => 'youtube',
+    'follow' => 'follow us',
+    'goShop' => 'go to our shop',
+    'powered' => 'powered by :name',
+    'continue_reading' => 'continue reading',
+    'wsuf_info' => 'Since 1881, a house belonged to a Christian family, the Batarsa family, and one of its corners was a church where prayers were held. The stones of the house were transported from Palestine on camels. The owners of the house moved out and it was abandoned for 24 years.',
+    'wsuf_food_quality' => 'The finest types of healthy foods made from natural ingredients can be found in the Beit Khairat Souq store.',
+
+];
---
+++ lang/en/news_.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'continue_reading' => 'continue reading',
+    'latest_news' => 'latest news',
+    'news_title' => 'news title',
+    'news_content' => 'Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news. Here we will write the details of the news'
+];
